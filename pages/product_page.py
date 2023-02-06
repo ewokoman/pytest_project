2aa856d975193b21216fae6d5121f6681f025e6a
Your Name <you@example.com>
2023-02-06 19:11:31 +0300

    lesson 4.3.2

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,4 +4,15 @@ from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
     def add_product_to_bucket(self):
-        pass
+        button_add_product = self.browser.find_element(*ProductPageLocators.BUTTON_BUY)
+        button_add_product.click()
+    
+    def check_product_name_add(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        add_product_name = self.browser.find_element(*ProductPageLocators.ADD_PRODUCT_TO_BUCKET).text
+        assert product_name==add_product_name, f"add {product_name}, but added {add_product_name}"
+
+    def check_product_price_add(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
+        bucket_price = self.browser.find_element(*ProductPageLocators.BUCKET_PRICE).text
+        assert product_price==bucket_price, f"add {product_price}, but added {bucket_price}"
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,13 @@
 from .pages.product_page import ProductPage
+import time
 
-link = 'http://selenium1py.pythonanywhere.com'
+link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
 
 def test_guest_can_add_product(browser):
     page = ProductPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.add_product_to_bucket()
+    page.solve_quiz_and_get_code()
+    page.check_product_name_add()
+    page.check_product_price_add()
+

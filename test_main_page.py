a7f2d8683accace5da18009fbaaef5330a9c5b38
Your Name <you@example.com>
2023-02-06 18:06:13 +0300

    update test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -6,9 +6,12 @@ link = 'http://selenium1py.pythonanywhere.com'
 link_form_auth = 'http://selenium1py.pythonanywhere.com/accounts/login/'
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()   
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
@@ -16,17 +19,19 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 
-def test_guest_should_be_login_url(browser):
-    page = LoginPage(browser, link_form_auth)
-    page.open()
-    page.should_be_login_url()
 
-def test_guest_should_be_login_form(browser):
-    page = LoginPage(browser, link_form_auth)
-    page.open()
-    page.should_be_login_form()
 
-def test_guest_should_be_register_for(browser):
-    page = LoginPage(browser, link_form_auth)
-    page.open()
-    page.should_be_register_form()
+# def test_guest_should_be_login_url(browser):
+#     page = LoginPage(browser, link_form_auth)
+#     page.open()
+#     page.should_be_login_url()
+
+# def test_guest_should_be_login_form(browser):
+#     page = LoginPage(browser, link_form_auth)
+#     page.open()
+#     page.should_be_login_form()
+
+# def test_guest_should_be_register_for(browser):
+#     page = LoginPage(browser, link_form_auth)
+#     page.open()
+#     page.should_be_register_form()

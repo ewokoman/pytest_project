09861ac5c8009f70d26d0a0bd0b712263c049b32
Your Name <you@example.com>
2023-02-07 15:31:46 +0300

    login_guest add mark

--- test_main_page.py
+++ test_main_page.py
@@ -3,13 +3,43 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import time
+import pytest
 
 
-# link = 'http://selenium1py.pythonanywhere.com'
-# link_form_auth = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self,browser):
+        link = 'http://selenium1py.pythonanywhere.com'
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com'
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
+def test_guest_should_be_login_url(browser):
+    link_form_auth = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+    page = LoginPage(browser, link_form_auth)
+    page.open()
+    page.should_be_login_url()
 
+def test_guest_should_be_login_form(browser):
+    link_form_auth = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+    page = LoginPage(browser, link_form_auth)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_be_register_for(browser):
+    link_form_auth = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+    page = LoginPage(browser, link_form_auth)
+    page.open()
+    page.should_be_register_form()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
@@ -19,37 +49,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket()
     page.busket_is_empty()
     page.text_basket_is_empty()
-    time.sleep(2)
-
-
-# def test_guest_can_go_to_login_page(browser):
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#     login_page = LoginPage(browser, browser.current_url)
-
-
-#     login_page.should_be_login_page()
-
-# def test_guest_should_see_login_link(browser):
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
-
-
-
-
-# def test_guest_should_be_login_url(browser):
-#     page = LoginPage(browser, link_form_auth)
-#     page.open()
-#     page.should_be_login_url()
-
-# def test_guest_should_be_login_form(browser):
-#     page = LoginPage(browser, link_form_auth)
-#     page.open()
-#     page.should_be_login_form()
-
-# def test_guest_should_be_register_for(browser):
-#     page = LoginPage(browser, link_form_auth)
-#     page.open()
-#     page.should_be_register_form()
